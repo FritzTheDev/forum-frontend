81eadb69d90cfb40c4a886583d5d6bb335eda790
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 09:16:26 -0800

    added placeholder reducer and removed extraneous import in app.js

---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,6 @@
+export default () => {
+    return {
+        username: 'OttoVBismarck',
+        email: 'fritzj96@gmail.com'
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-
+import AuthReducer from './auth_reducer';
 export default combineReducers({
-    
+    auth: AuthReducer
 });
