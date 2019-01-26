db21ab8ee0f73413076dacd81a6869da40d9c3db
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 09:10:23 -0800

    added a reducer index and created an "App" component

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+
+const App = () => {
+  return(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <BrowserRouter>
+      
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App;
