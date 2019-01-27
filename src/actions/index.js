25c75dc7dbe080822b55c4d1de02f0ed8147c5e5
Fritz Johnson <fritzj96@gmail.com>
2019-01-27 01:35:09 -0800

    action creator progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,13 @@
 import { userConstants } from '../constants/user_constants';
+import { bindActionCreators } from '../../../../../AppData/Local/Microsoft/TypeScript/3.2/node_modules/redux';
 
 const { LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT} = userConstants;
 
-export const attemptLogin = () => {
-    return {
-        type: LOGIN_REQUEST
+export const attemptLogin = (username, password) => {
+    return dispatch => {
+        dispatch(request({ username }));
+
+        
+
     }
 }
