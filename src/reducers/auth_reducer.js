3ff00ef2a253b75542f248ae59a7b0431760869e
Fritz Johnson <fritzj96@gmail.com>
2019-01-27 06:14:24 -0800

    created auth reducer

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,6 +1,28 @@
-export default () => {
-    return {
-        username: 'OttoVBismarck',
-        email: 'fritzj96@gmail.com'
+import { userConstants } from '../constants/user_constants';
+
+let user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { loggedIn: true, user }  : {};
+
+export default function AuthReducer(state = initialState, action) {
+    switch (action.type) {
+    case userConstants.LOGIN_REQUEST: {
+        console.log(action);
+        return {
+            loggingIn: true,
+            user: action.user
+        };
+    }
+    case userConstants.LOGIN_SUCCESS: {
+        return {
+            loggedIn: true,
+            user: action.user
+        };
+    }
+    case userConstants.LOGIN_FAILURE:
+        return {};
+    case userConstants.LOGOUT:
+        return {};
+    default:
+        return state;
     }
 }
