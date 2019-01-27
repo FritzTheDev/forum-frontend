ee916e42fb807ce45ecb9c9c5ecc13e9481ae83e
Fritz Johnson <fritzj96@gmail.com>
2019-01-26 20:00:42 -0800

    Lots of navbar progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Navbar = (props) => {
-    return (
-        <div>Test</div>
-    )
+import {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
+    MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon
+} from "mdbreact";
+
+class NavbarPage extends Component {
+    state = { isOpen: false, user: { username: 'Fritz'} };
+
+    toggleCollapse = () => {
+        this.setState({ isOpen: !this.state.isOpen });
+    }
+
+    render() {
+        return (
+            <MDBNavbar color="indigo" dark expand="md">
+                <MDBNavbarBrand>
+                    <strong className="white-text">Forum Example</strong>
+                </MDBNavbarBrand>
+                <MDBNavbarToggler onClick={this.toggleCollapse} />
+                <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+                    {this.renderAuthSection()}
+                </MDBCollapse>
+            </MDBNavbar>
+        )
+    }
+
+    renderAuthSection() {
+        if (this.state.user) {
+            return (
+                <MDBNavbarNav right>
+                    <MDBNavItem>
+                        <MDBNavLink to="profile">Signed in as {this.state.user.username}</MDBNavLink>
+                    </MDBNavItem>
+                </MDBNavbarNav>
+            )
+        }
+    }
 }
 
-export default Navbar;
+export default NavbarPage;
